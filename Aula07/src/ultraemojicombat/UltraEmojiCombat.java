1de0a68324baa5e14f821cc4aa5eb1d5f507febb
Zatvello <37464317+Zatvello@users.noreply.github.com>
2023-11-09 07:45:31 -0300

    Lutador l[0] atualizado

--- Aula07/src/ultraemojicombat/UltraEmojiCombat.java
+++ Aula07/src/ultraemojicombat/UltraEmojiCombat.java
@@ -5,7 +5,7 @@ public class UltraEmojiCombat {
     public static void main(String[] args) {
         Lutador l[] = new Lutador[6];
 
-        l[0] = new Lutador("Pretty Boy","França", 31,1.75f, 52.2f, 11, 3 , 1);
+        l[0] = new Lutador("Pretty Boy","França", 31,1.75f, 68.9f, 11, 3 , 1);
         l[0].apresentar();
         l[0].status();
     }
