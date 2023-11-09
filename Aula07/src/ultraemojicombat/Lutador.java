5aa56995ccf23a9e280a086be4be92176ad0a359
Zatvello <37464317+Zatvello@users.noreply.github.com>
2023-11-09 07:44:31 -0300

    UEC 2.0 - Aula 07

--- Aula07/src/ultraemojicombat/Lutador.java
+++ Aula07/src/ultraemojicombat/Lutador.java
@@ -1,6 +1,7 @@
 package ultraemojicombat;
 
 public class Lutador {
+    // Atributos
     private String nome;
     private String nacionalidade;
     private int idade;
@@ -10,4 +11,140 @@ public class Lutador {
     private int vitorias;
     private int derrotas;
     private int empates;
+
+
+    // Método Construtor
+    public Lutador(String no, String na, int id, float al, float pe, int vi, int de, int em) {
+        this.nome = no;
+        this.nacionalidade = na;
+        this.idade = id;
+        this.altura = al;
+        setPeso(pe); //Obrigatório utilizar nesse exercício
+        this.vitorias = vi;
+        this.derrotas = de;
+        this.empates = em;
+    }
+
+    // Métodos Públicos
+    public void apresentar() {
+        System.out.println("-------> Apresentação <-------");
+        System.out.println("Lutador: " + this.getNome());
+        System.out.println("Origem: " + this.nacionalidade);
+        System.out.println(this.getIdade() + " anos");
+        System.out.println(this.getAltura() + "m de altura");
+        System.out.println("Pesando: " + this.getPeso() + "kg");
+        System.out.println("Ganhou: " + this.getVitorias());
+        System.out.println("Perdeu: " + this.getDerrotas());
+        System.out.println("Empatou: " + this.empates);
+    }
+
+    public void status() {
+        System.out.println("------------------------------");
+        System.out.println(this.getNome());
+        System.out.println("é um peso " + this.getCategoria());
+        System.out.println(this.getVitorias() + " vitórias");
+        System.out.println(this.getDerrotas() + " derrotas");
+        System.out.println(this.getEmpates() + " empates");
+    }
+
+    public void ganharLuta() {
+        this.setVitorias(this.getVitorias() + 1);
+    }
+
+    public void perderLuta() {
+        this.setDerrotas(this.getDerrotas() + 1);
+    }
+
+    public void empatarLuta() {
+        this.setEmpates(this.getEmpates() + 1);
+    }
+
+    // Métodos especiais
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String no) {
+        this.nome = no;
+    }
+
+    public String getNacio() {
+        return this.nacionalidade;
+    }
+
+    public void setNacio(String na) {
+        this.nacionalidade = na;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int id) {
+        this.idade = id;
+    }
+
+    public float getAltura() {
+        return this.altura;
+    }
+
+    public void setAltura(float al) {
+        this.altura = al;
+    }
+
+    public float getPeso(){
+        return this.peso;
+    }
+
+    public void setPeso(float pe) {
+        this.peso = pe;
+        setCategoria();
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    public void setCategoria() {
+        if (this.peso < 52.2) {
+            this.categoria = "Inválido";
+
+        } else if (this.peso <= 70.3) {
+            this.categoria = "Leve";
+
+        } else if (this.peso <= 83.9) {
+            this.categoria = "Médio";
+
+        } else if (this.peso <= 120.2) {
+            this.categoria = "Pesado";
+
+        } else {
+            categoria = "Inválido";
+        }
+    }
+
+    public int getVitorias() {
+        return this.vitorias;
+    }
+
+    public void setVitorias(int vi) {
+        this.vitorias = vi;
+    }
+
+    public int getDerrotas() {
+        return this.derrotas;
+    }
+
+    public void setDerrotas(int de) {
+        this.derrotas = de;
+    }
+
+    public int getEmpates() {
+        return this.derrotas;
+    }
+
+    public void setEmpates(int em) {
+        this.empates = em;
+    }
 }
--- Aula07/src/ultraemojicombat/UltraEmojiCombat.java
+++ Aula07/src/ultraemojicombat/UltraEmojiCombat.java
@@ -3,6 +3,10 @@ package ultraemojicombat;
 public class UltraEmojiCombat {
 
     public static void main(String[] args) {
+        Lutador l[] = new Lutador[6];
 
+        l[0] = new Lutador("Pretty Boy","França", 31,1.75f, 52.2f, 11, 3 , 1);
+        l[0].apresentar();
+        l[0].status();
     }
 }
