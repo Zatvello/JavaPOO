86eb83c3330a69af5192335318316dae6131335d
Zatvello <37464317+Zatvello@users.noreply.github.com>
2023-11-26 02:33:20 -0300

    Aula 14 - Projeto Youtube

---
+++ ProjetoYoutube/src/projetoyoutube/ProjetoYoutube.java
@@ -0,0 +1,4 @@
+package projetoyoutube;
+
+public class ProjetoYoutube {
+}
---
+++ ProjetoYoutube/src/projetoyoutube/Video.java
@@ -0,0 +1,18 @@
+package projetoyoutube;
+
+public class Video implements AcoesVideo{
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void like() {
+
+    }
+}
