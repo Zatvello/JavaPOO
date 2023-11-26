2fbaa8fbb001ee5a15a33c633f80ef9c1a749bed
Zatvello <37464317+Zatvello@users.noreply.github.com>
2023-11-26 02:52:02 -0300

    Classes/Interfaces Gafanhoto e Pessoa criadas.

---
+++ ProjetoYoutube/src/projetoyoutube/Gafanhoto.java
@@ -0,0 +1,26 @@
+package projetoyoutube;
+
+public class Gafanhoto extends Pessoa {
+    private String login;
+    private int totAssistido;
+
+    public void viuMaisUm() {
+
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotAssistido() {
+        return totAssistido;
+    }
+
+    public void setTotAssistido(int totAssistido) {
+        this.totAssistido = totAssistido;
+    }
+}
--- ProjetoYoutube/src/projetoyoutube/ProjetoYoutube.java
+++ ProjetoYoutube/src/projetoyoutube/ProjetoYoutube.java
@@ -1,4 +1,11 @@
 package projetoyoutube;
 
 public class ProjetoYoutube {
+    public static void main(String[] args) {
+        Video[] v = new Video[3];
+        v[1] = new Video("Oi");
+        System.out.println(v[1].getAvaliacao());
+        System.out.println(v[1].getTitulo());
+        System.out.println>>(v[1].toString());
+    }
 }
